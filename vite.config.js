04ed4245a85ff7a01b05453d95616e353e4545b7
charlieforward9 <charlesrichardsonusa@gmail.com>
2024-06-23 21:48:33 -0500

    chore: update deps/config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    hmr:
-      process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
-        ? 443
-        : undefined,
-  },
+export default defineConfig(({ mode }) => {
+  const { GOOGLE_MAPS_API_KEY = "" } = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      "process.env.GOOGLE_MAPS_API_KEY": JSON.stringify(GOOGLE_MAPS_API_KEY),
+    },
+    resolve: {
+      alias: {
+        "@vis.gl/react-google-maps/examples.js":
+          "https://visgl.github.io/react-google-maps/scripts/examples.js",
+      },
+    },
+  };
 });
