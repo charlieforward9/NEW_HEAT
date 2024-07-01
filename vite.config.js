8e9df1fd539d7cf67b568855f3af4b3bc274b3ba
charlieforward9 <charlesrichardsonusa@gmail.com>
2024-06-30 23:10:58 -0500

    chore(config): set build settings

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import MillionLint from "@million/lint";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => {
-  const { GOOGLE_MAPS_API_KEY = "" } = loadEnv(mode, process.cwd(), "");
+  const { VITE_GOOGLE_MAPS_API_KEY = "" } = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [MillionLint.vite(), react()],
     define: {
-      "process.env.GOOGLE_MAPS_API_KEY": JSON.stringify(GOOGLE_MAPS_API_KEY),
+      "process.env.GOOGLE_MAPS_API_KEY": JSON.stringify(
+        VITE_GOOGLE_MAPS_API_KEY
+      ),
     },
     resolve: {
       alias: {
